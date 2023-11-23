5ac544b0fdadcd3e8405d5985b9a6e3d9e12ed37
Fredy Yoel Vasquez Miranda <119565618+fredy16v@users.noreply.github.com>
2023-11-22 22:16:46 -0600

    Configuracion Identity (#2)

--- TicketFlow/Program.cs
+++ TicketFlow/Program.cs
@@ -1,4 +1,6 @@
+using Microsoft.AspNetCore.Identity;
 using TicketFlow;
+using TicketFlow.DB.Seeders;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,5 +11,24 @@ startup.ConfigureServices(builder.Services);
 var app = builder.Build();
 
 startup.Configure(app, app.Environment);
+
+using (var scope = app.Services.CreateScope())//necesario para usar el identity
+{
+    var service = scope.ServiceProvider;
+    var loggerFactory = service.GetRequiredService<ILoggerFactory>();
+    
+    try
+    {
+        var userManager = service.GetRequiredService<UserManager<IdentityUser>>();
+        var roleManager = service.GetRequiredService<RoleManager<IdentityRole>>();
+        
+        await UsersRolesSeeder.LoadDataAsync(userManager, roleManager, loggerFactory);
+    }
+    catch (Exception e)
+    {
+        var logger = loggerFactory.CreateLogger<Program>();
+        logger.LogError(e, "Ocurrió un error al migrar o al insertar los datos");
+    }
+}
 
 app.Run();
