91f0e3a5106ae8a381ed8d94b5106beba6a894ae
fredy16v <fredy16v@gmail.com>
2023-11-22 21:44:46 -0600

    Configuracion de entity

---
+++ TicketFlow/DB/Seeders/UsersRolesSeeder.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Identity;
+using TicketFlow.DB.Contexts;
+
+namespace TicketFlow.DB.Seeders;
+
+public static class UsersRolesSeeder
+{
+    public static async Task LoadDataAsync(
+        UserManager<IdentityUser> userManager,
+        RoleManager<IdentityRole> roleManager,
+        ILoggerFactory loggerFactory)
+    {
+        try
+        {
+            if(!roleManager.Roles.Any())
+            {
+                await roleManager.CreateAsync(new IdentityRole("Admin"));
+                await roleManager.CreateAsync(new IdentityRole("User"));
+            }
+
+            if (!userManager.Users.Any())
+            {
+                var userAdmin = new IdentityUser
+                {
+                    UserName = "jperez",
+                    Email = "jperez@me.com"
+                };
+                await userManager.CreateAsync(userAdmin, "Temporal01#");
+                await userManager.AddToRoleAsync(userAdmin, "Admin");
+                
+                var normalUser = new IdentityUser
+                {
+                    UserName = "pepito@me.com",
+                    Email = "pepito@me.com"
+                };
+                await userManager.CreateAsync(normalUser, "Temporal01#");
+                await userManager.AddToRoleAsync(normalUser, "User");
+            }
+        }
+        catch (Exception e)
+        {
+            var logger = loggerFactory.CreateLogger<ApplicationDbContext>();
+            logger.LogError(e.Message);
+            throw;
+        }
+    }
+}
---
+++ TicketFlow/Startup.cs
@@ -0,0 +1,79 @@
+﻿using System.Text;
+using System.Text.Json.Serialization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using TicketFlow.DB.Contexts;
+
+namespace TicketFlow;
+
+public class Startup
+{
+    public Startup(IConfiguration configuration)
+    {
+        Configuration = configuration;
+    }
+
+    public IConfiguration Configuration { get; }
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddControllers().AddJsonOptions(x => x.JsonSerializerOptions.ReferenceHandler
+        = ReferenceHandler.IgnoreCycles);// para solucionar el error de entra en bucle el sql porque hay una relacion de muchos a muchos
+        
+        //Add DbContext
+        services.AddDbContext<ApplicationDbContext>(options =>
+        {
+            options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
+        });
+
+        services.AddAutoMapper(typeof(Startup));
+        services.AddHttpContextAccessor();
+        
+        //Add Identity
+        services.AddIdentity<IdentityUser, IdentityRole>(options =>
+            {
+                options.SignIn.RequireConfirmedAccount = false;
+            }).AddEntityFrameworkStores<ApplicationDbContext>().AddDefaultTokenProviders();
+        
+        // Add cache filter
+        services.AddResponseCaching();
+
+		//Add CORS
+		services.AddCors(options =>
+        {
+            options.AddPolicy("CorsRule", rule =>
+            {
+                rule.AllowAnyHeader().AllowAnyMethod().WithOrigins("*");
+            });
+            
+        });// para permitir que se conecte el backend con el forntend
+        
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen();
+    }
+
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        //if (env.IsDevelopment())
+        //{
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        //}
+
+        app.UseHttpsRedirection();
+
+        app.UseRouting();
+
+        app.UseResponseCaching();
+        
+        app.UseCors("CorsRule");
+        
+        app.UseAuthorization();
+
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapControllers();
+        });
+    }
+}
