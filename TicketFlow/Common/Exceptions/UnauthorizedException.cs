51918d10aaaa5335dcb1d17a65fc43ed912a7035
Erick Arita <erick.emao@gmail.com>
2023-11-28 02:11:58 -0600

    Add: Creado controller de clientes para probar que se haga reflexion

---
+++ TicketFlow/Common/Exceptions/UnauthorizedException.cs
@@ -0,0 +1,11 @@
+﻿namespace TicketFlow.Common.Exceptions;
+
+public class UnauthorizedException : Exception
+{
+    public object Errors { get; }
+
+    public UnauthorizedException(object errors) : base(errors.ToString())
+    {
+        Errors = errors;
+    }
+}
---
+++ TicketFlow/Core/Customer/ICustomerService.cs
@@ -0,0 +1,16 @@
+﻿using TicketFlow.Core.Customer.Dtos;
+
+namespace TicketFlow.Core.Customer;
+
+public interface ICustomerService
+{
+    Task<CustomerResponse> AddAsync(CreateCustomerRequest createCustomerRequest);
+
+    Task<CustomerResponse> GetByIdAsync(Guid id);
+
+    Task<IReadOnlyCollection<CustomerResponse>> GetAllAsync();
+
+    Task UpdateAsync(UpdateCustomerRequest updateCustomerRequest);
+
+    Task DeleteAsync(Guid id);
+}
