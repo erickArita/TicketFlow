b6065bc8bf3e3ba4ed1a00d395492013bc0f6b9b
fredy16v <fredy16v@gmail.com>
2023-12-06 21:47:07 -0600

    fix error de verbo http

--- TicketFlow/Controllers/TicketsController.cs
+++ TicketFlow/Controllers/TicketsController.cs
@@ -85,9 +85,9 @@ public class TicketsController : ControllerBase
     }
 
     /// <summary>
-    ///     Reemplaza al usuaruio encargado del ticket, solo para el rol de admin
+    ///     Reemplaza al usuario encargado del ticket, solo para el rol de admin
     /// </summary>
-    [HttpPost("{ticketId}/users/{userId}")]
+    [HttpPut("{ticketId}/users/{userId}")]
     [Authorize(Roles = nameof(Roles.Admin))]
     public async Task<ActionResult<AplicationResponse<TicketResponse>>> AddUser2Ticket(Guid ticketId, Guid userId)
     {
